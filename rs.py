9582dafe18e0a81d1bf483aaf0a121b65558849a
Ruslan321532 <ruslanburalkiev8@gmail.com>
2023-04-17 21:58:29 +0600

    new dznumber4

---
+++ rs.py
@@ -0,0 +1,29 @@
+# class Raser:
+#     def drive(self):
+#         print("Drive very fast")
+
+# class Car:
+#     def drive(self):
+#         print("drive")
+
+# class RacingCar(Car, Raser):
+#     pass
+
+# car = RacingCar()
+# car.drive()
+
+
+# class Car:
+#     wheels = 5
+
+
+# class Track(Car):
+#     wheels = 10
+
+
+# man = Track()
+# Track.wheels = 8
+# kamaz = Track()
+# print(man.wheels + kamaz.wheels + Car.wheels)
+
+
